0865e3374dea21be6b1a113596010c467f805adc
dongamaxime-tech <dongamaxime@gmail.com>
2026-05-21 23:03:26 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,8 @@
 import os, sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from serveur_web import app, setup_webhook
-import threading
+from serveur_web import app
 
 if __name__ == "__main__":
-    t = threading.Thread(target=setup_webhook, daemon=True)
-    t.start()
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
