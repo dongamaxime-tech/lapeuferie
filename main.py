69ae7672a069dd339f6fcb1187b7beae77c4e6f7
dongamaxime-tech <dongamaxime@gmail.com>
2026-06-25 10:42:17 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os, sys
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from serveur_web import app
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False)
